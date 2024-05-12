a1db83e43c7ef08db02ff16d48b197210c9ec632
Jasiri <jasiridickson@gmail.com>
2024-05-11 23:28:17 -0500

    Creating userrepository interface

--- src/main/java/com/jasirid/demo/repository/UserRepository.java
+++ src/main/java/com/jasirid/demo/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.jasirid.demo.repository;
 
-public interface UserRepository {
+import com.jasirid.demo.entity.User;
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Extending JpaRepository interface. We pass the type of the entity (user class) and the data class of the id (int)
+//I believe this allows the UserRepository interface to perform CRUD operations on our User Jpa entity.
+//There is no @repository annotation here because JpaRepository already contains it.
+public interface UserRepository extends JpaRepository <User, Integer>{
+
 }
