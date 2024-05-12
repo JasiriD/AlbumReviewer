2b456b33635009a729c487dc44a0e5c29a068ae5
Jasiri <jasiridickson@gmail.com>
2024-05-11 23:41:57 -0500

    Creating UserDTO class

---
+++ src/main/java/com/jasirid/demo/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.jasirid.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//Lombok annotations
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    //adding fields from User class
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String Password;
+    private String email;
+}
