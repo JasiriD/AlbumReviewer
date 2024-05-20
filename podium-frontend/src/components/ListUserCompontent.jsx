853eea0bb3f8282025654df597c081d946ed53e2
Jasiri <jasiridickson@gmail.com>
2024-05-20 09:43:34 -0500

    Setting up a table to display users

---
+++ podium-frontend/src/components/ListUserCompontent.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function ListUserCompontent() {
+
+    const testData = [
+        {
+            "id":1,
+            "firstName": "Jasiri",
+            "lastName": "Dickson",
+            "userName": "Jeezi",
+            "email": "Shingun121@gmail.com"
+        },
+        {
+            "id":1,
+            "firstName": "Jasiri2",
+            "lastName": "Dickson2",
+            "userName": "Jeezi2",
+            "email": "Shingun121@gmail.com2"
+        },
+        {
+            "id":3,
+            "firstName": "Jasiri3",
+            "lastName": "Dickson3",
+            "userName": "Jeezi3",
+            "email": "Shingun121@gmail.com3"
+        },
+    ]
+
+  return (
+    <div>ListUserCompontent</div>
+  )
+}
+
+export default ListUserCompontent
