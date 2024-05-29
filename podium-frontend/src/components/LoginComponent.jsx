8e6414273d59b64a86293d3f41f1315541dfc5d4
Jasiri <jasiridickson@gmail.com>
2024-05-29 10:55:35 -0500

    LoginComponent now has the proper layout within react

--- podium-frontend/src/components/LoginComponent.jsx
+++ podium-frontend/src/components/LoginComponent.jsx
@@ -1,9 +1,34 @@
 //rafce is the command to create a simple baseline react component
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 export const LoginComponent = () => {
+
+  const navigate = useNavigate();
+
+
+  //loginAttempt function
+  function loginAttempt(){
+
+  }
+
   return (
-    <div>LoginComponent</div>
+    <div className='container'>
+      <div className = 'card col-md-6 offset-md-3'>
+        <div className='card-body'>
+          <form>
+            <div className='form-group'>
+              <label className='form-label'>Username</label>
+              <input type="text" className="form-control" id="inputUsername1" placeholder="Enter username"></input>
+            </div>
+            <div className='form-group'>
+              <label className='form-label'>Password</label>
+              <input type="text" className="form-control" id="inputPassword1" placeholder="Enter password"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
