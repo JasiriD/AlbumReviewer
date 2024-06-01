4416ac0746bbfc4fd318b048422eb2c9b8dcf3ce
Jasiri <jasiridickson@gmail.com>
2024-06-01 06:51:46 -0500

    Created reviewWriteComponent, a component that contains the write review form. Originally was done in the homepage but that leads to hard to read code

--- podium-frontend/src/components/ReviewWriteComponent.jsx
+++ podium-frontend/src/components/ReviewWriteComponent.jsx
@@ -2,7 +2,53 @@ import React from 'react'
 
 const ReviewWriteComponent = () => {
   return (
-    <div>ReviewWriteComponent</div>
+    <div>
+         <h3 className='text-center'>Write New Review</h3>
+            <form>
+                {/* Field for review title */}
+                <div className='form-group'>
+                    <label className='form-label text-center'>Album Title:</label>
+                    <input
+                    type='text'
+                    name='atitle'
+                    className={`form-control ${errors.atitle ? 'is-invalid': ''}`}
+                    //Changes userName in useState variable immediately something is typed in form
+                    onChange={handleaTitle}
+                    ></input>
+
+                </div>
+                {/* Field for album title*/}
+                <div className='form-group'>
+                    <label className='form-label text-center'>Review Title:</label>
+                    <input
+                    type='text'
+                    name='title'
+                    className={`form-control ${errors.title ? 'is-invalid': ''}`}
+                    onChange={handleTitle}
+                    ></input>
+
+                </div>
+                {/* Field for review body*/}
+                <div className='form-group'>
+                    <label className='form-label text-center'>Review:</label>
+                    
+                    <textarea
+                    type='text'
+                    name='body'
+                    className={`form-control ${errors.body ? 'is-invalid': ''}`}
+                    rows={7}
+                    onChange={handleBody}
+                    ></textarea>
+
+                </div>
+                <br/>   
+                {/*Submit button*/}
+                <div className='text-center'>
+                    <button className='btn btn-success' onClick={submitReview}>New Review</button>
+                </div>  
+                
+            </form>
+    </div>
   )
 }
 
